927d8f6919f09ee88ae718fb40e94f51fab41ead
GoX2Maker <gox2maker@gmail.com>
2023-11-05 14:48:34 +0900

    Fix code - Sign up 디자인 추가

---
+++ tiktok_clone/lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.grey.shade200,
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: Sizes.size24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already have an account?"),
+                Gaps.h5,
+                Text(
+                  "Log in",
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          )),
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(children: [
+            Gaps.v80,
+            Text(
+              "Sign Up for TikTok",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v20,
+            Text(
+              "Create a profile, follow other accounts, make your own videos, and more.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black45,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
